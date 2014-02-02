5def10655ec6fea8c4f1b8329f843feb20e4dccc
Kevin Swiber <kswiber@gmail.com>
2014-02-01 19:45:04 -0500

    Adding common hypermedia type names and values.

---
+++ hypermedia_types.js
@@ -0,0 +1,13 @@
+module.exports = Object.freeze({
+  SIREN: 'application/vnd.siren+json',
+  HAL: 'application/hal+json',
+  HAL_XML: 'application/hal+xml',
+  COLLECTION: 'application/vnd.collection+json',
+  HTML: 'text/html',
+  XHTML: 'application/xhtml+xml',
+  ATOM: 'application/atom+xml',
+  JSON: 'application/json', // *
+  XML: 'application/xml' // *
+});
+
+// * Not really hypermedia, but comes in handy sometimes.
