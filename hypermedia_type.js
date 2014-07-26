a7532c8362ebf5c339a05c78c544d5803659ae0a
Kevin Swiber <kswiber@gmail.com>
2014-07-26 10:46:49 -0400

    Adding DocJSON.  Fixes #1.

--- hypermedia_type.js
+++ hypermedia_type.js
@@ -1,6 +1,7 @@
 module.exports = Object.freeze({
   ATOM: 'application/atom+xml',
   COLLECTION: 'application/vnd.collection+json',
+  DOCJSON: 'application/vnd.document+json',
   HAL: 'application/hal+json',
   HAL_XML: 'application/hal+xml',
   HTML: 'text/html',
